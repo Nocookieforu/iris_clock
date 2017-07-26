3ec4dfa2e34d66fbce88e121772a9d0668301e34
Nocookieforu <aicantarxd@gmail.com>
2017-07-25 20:49:01 -0500

    Add method to show time

--- iris_clock.ino
+++ iris_clock.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include <Time.h>
 
 #define PIN 17
 
@@ -10,18 +11,39 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();
   // Initialize all pixels to 'off'
   strip.show();
+  strip.setBrightness(32);
 }
 
 void loop() {
   // Some example procedures showing how to display to the pixels:
-  rainbow(20);
-  rainbowCycle(20);
+  //rainbow(20);
+  //rainbowCycle(20);
+
+  // Increase time by one second
+  adjustTime(1);
+  // Show time on LEDs
+  display_time(now());
+  //delay(1000);
+  delay(100);
+}
+
+void display_time(time_t time)
+{
+  uint8_t i;
+  for (i = 0; i < 8; i++)
+  {
+    uint8_t hour_bit = (hour(time)   >> i) & 0x1;
+    uint8_t min_bit  = (minute(time) >> i) & 0x1;
+    uint8_t sec_bit  = (second(time) >> i) & 0x1;
+    strip.setPixelColor(i, 255*hour_bit, 255*min_bit, 255*sec_bit);
+  }
+  strip.show();
 }
 
 void rainbow(uint8_t wait) {
